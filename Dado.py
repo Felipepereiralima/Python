21ddb32e501607f5dabcc24a8395d8bd0be135ad
Felipe Lima <felipeplima@live.com>
2021-07-16 16:34:52 -0300

    Atualização Dado

--- Dado.py
+++ Dado.py
@@ -1,10 +1,11 @@
 from random import randint
-Pergunta = 'Não'
-Pergunta = str(input('Gostaria de jogar o dado Sim/Não? ')).strip().upper()
-while Pergunta in 'Sim sim s':
-    Pergunta = str(input('Gostaria de jogar o dado Sim/Não? '))
-    if Pergunta in 'Sim sim':
-        Numero = int(randint(1, 6))
-        print('Seu número do dado é {}.'.format(Numero))
-    else:
-        print('Que pena, fica pra próxima.')
+while True:
+    pergunta = ' '
+    while pergunta not in 'SN':
+        pergunta = str(input('Gostaria de jogar o dado Sim/Não? ')).strip().upper()[0]
+    if pergunta == 'S':
+        número = randint(1, 6)
+        print(f'Seu número do dado é {número}.')
+    if pergunta == 'N':
+        print('Obrigado por usar o programa dado.')
+        break
